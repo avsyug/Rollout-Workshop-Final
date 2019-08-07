5ddc64571c549782172c1ddc2c5d0a8338e81ea5
Greg Avsyuk <gavsyuk413@gmail.com>
2019-08-07 17:11:37 -0400

    Update InstructorApp.jsx
    Add navbar

--- frontend-spring-boot-react-crud-full-stack-with-maven/src/component/InstructorApp.jsx
+++ frontend-spring-boot-react-crud-full-stack-with-maven/src/component/InstructorApp.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ListCoursesComponent from './ListCoursesComponent';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import CourseComponent from './CourseComponent';
+import { Button, Navbar } from 'react-bootstrap';
 import Flags from '../flags.js';
 
 class InstructorApp extends Component {
@@ -9,7 +10,9 @@ class InstructorApp extends Component {
         return (
             <Router>
                 <>
-                    <h1>Instructor Application</h1>
+                    <Navbar bg="light">
+                        <Navbar.Brand href="../courses">Instruction Application Tutorial</Navbar.Brand>
+                    </Navbar>
                     <Switch>
                         <Route path="/" exact component={ListCoursesComponent} />
                         <Route path="/courses" exact component={ListCoursesComponent} />
