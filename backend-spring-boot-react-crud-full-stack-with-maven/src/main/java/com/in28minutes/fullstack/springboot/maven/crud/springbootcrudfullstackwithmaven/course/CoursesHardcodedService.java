2c5033751be675352f694d8213653ad5cf710e1d
Greg Avsyuk <gavsyuk413@gmail.com>
2019-08-12 13:15:52 -0400

    Update CoursesHardcodedService.java

--- backend-spring-boot-react-crud-full-stack-with-maven/src/main/java/com/in28minutes/fullstack/springboot/maven/crud/springbootcrudfullstackwithmaven/course/CoursesHardcodedService.java
+++ backend-spring-boot-react-crud-full-stack-with-maven/src/main/java/com/in28minutes/fullstack/springboot/maven/crud/springbootcrudfullstackwithmaven/course/CoursesHardcodedService.java
@@ -15,6 +15,7 @@ public class CoursesHardcodedService {
 
 	private static List<Course> courses = new ArrayList<>();
 	private static long idCounter = 0;
+	private static FlagsContainer conf = new FlagsContainer();
 
 	static {
 		courses.add(new Course(++idCounter, "in28minutes", "Learn Full stack with Spring Boot and Angular"));
@@ -22,8 +23,6 @@ public class CoursesHardcodedService {
 		courses.add(new Course(++idCounter, "in28minutes", "Master Microservices with Spring Boot and Spring Cloud"));
 		courses.add(new Course(++idCounter, "in28minutes",
 				"Deploy Spring Boot Microservices to Cloud with Docker and Kubernetes"));
-
-		FlagsContainer conf = new FlagsContainer();
 		try {
 			Rox.register("default", conf);
 			Rox.setup("").get();
